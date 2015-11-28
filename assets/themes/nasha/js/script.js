82e230a8eb90a89f2c534d35cbfad62577802840
carney520 <carney520@hotmail.com>
2015-11-28 21:42:22 +0800

    add image displayer

--- assets/themes/nasha/js/script.js
+++ assets/themes/nasha/js/script.js
@@ -5,4 +5,28 @@ $(document).ready(function(){
       document.location="#markdown-toc";
     });
   }
+
+  //the image displayer
+  $('.post-content img').css({cursor:'pointer'}).click(function(event){
+    //get image source
+    var href=this.src;
+    var displayer=$('<div></div>').addClass('image-displayer').insertAfter($('body'));
+    $('body').css('overflow','hidden');
+    $('<img/>').attr('src',href).appendTo(displayer).toggleClass('image-fit').click(function(){
+      $(this).toggleClass('image-fit');
+      if(!$(this).hasClass('image-fit')){
+        $(this).css('cursor','zoom-out');
+      }else{
+        $(this).css('cursor','zoom-in');
+      }
+    });
+
+    //the close button
+    $('<div>Close</div>').addClass('display-close').appendTo(displayer).click(function(){
+      //remove image displayer
+      displayer.remove()
+      $('body').css('overflow','scroll');
+    });
+  }
+  );
 });
